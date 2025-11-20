fe16b42d08e809b80dcca94f95808b150077d5ab
YourGitHubUsername <your-email@example.com>
2025-11-20 08:33:02 -0500

    Implement search functionality and style SearchPage.

--- giftlink-frontend/src/components/Navbar/Navbar.js
+++ giftlink-frontend/src/components/Navbar/Navbar.js
@@ -1,19 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom"; // Import Link
 
 export default function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="/">GiftLink</a>
+      <Link className="navbar-brand" to="/">GiftLink</Link> {/* Changed to Link */}
 
       <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav">
 
           <li className="nav-item">
-            <a className="nav-link" href="/home.html">Home</a>
+            <Link className="nav-link" to="/home">Home</Link> {/* Changed to Link */}
           </li>
 
           <li className="nav-item">
-            <a className="nav-link" href="/app">Gifts</a>
+            <Link className="nav-link" to="/app">Gifts</Link> {/* Changed to Link */}
+          </li>
+          
+          <li className="nav-item">
+            <Link className="nav-link" to="/app/search">Search</Link> {/* Fixed: changed a to Link and href to to */}
           </li>
 
         </ul>
